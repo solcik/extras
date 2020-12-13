f65fc5449b768ff5adbb7a2243213d5bccf64d25
David Šolc <solcik@gmail.com>
2020-12-13 13:54:55 +0100

    nette: forms controls; doctrine: fixtures

---
+++ src/Nette/Forms/Controls/ZonedDateTimeInput.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solcik\Nette\Forms\Controls;
+
+use Brick\DateTime\LocalDateTime;
+use Brick\DateTime\TimeZone;
+use Brick\DateTime\ZonedDateTime;
+
+class ZonedDateTimeInput extends LocalDateTimeInput
+{
+    public static string $timezone = 'Europe/Prague';
+
+    public function getValue()
+    {
+        $val = parent::getValue();
+        if (!$this->isValidated) {
+            return $val;
+        }
+
+        if ($val === null || $val === '') {
+            return null;
+        }
+
+        assert($val instanceof LocalDateTime);
+
+        return $val->atTimeZone(TimeZone::parse(self::$timezone));
+    }
+
+    public function setValue($value)
+    {
+        if ($value === null) {
+            parent::setValue(null);
+        } elseif ($value instanceof ZonedDateTime) {
+            parent::setValue($value->getDateTime()->__toString());
+
+            $this->validate();
+        } else {
+            parent::setValue($value);
+        }
+
+        return $this;
+    }
+}
