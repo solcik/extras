6088586c9c0030bc8c89e1bc352a6e22d36eb8c9
David Solc <solcik@gmail.com>
2022-03-02 11:18:26 +0100

    composer: update

--- ecs.php
+++ ecs.php
@@ -2,31 +2,27 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
-use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
 use SlevomatCodingStandard\Sniffs\Commenting\DisallowCommentAfterCodeSniff;
-use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
+use SlevomatCodingStandard\Sniffs\Functions\RequireTrailingCommaInDeclarationSniff;
 use SlevomatCodingStandard\Sniffs\PHP\DisallowDirectMagicInvokeCallSniff;
-use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff;
-use SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff;
-use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\Naming\StandardizeHereNowDocKeywordFixer;
 use Symplify\CodingStandard\Fixer\Spacing\MethodChainingNewlineFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $c): void {
-    $c->import(__DIR__ . '/vendor/symplify/coding-standard/config/symplify.php');
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/clean-code.php');
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/common/*.php');
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/dead-code.php');
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php70.php');
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php71.php');
-    $c->import(
-        __DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php_cs_fixer/php-cs-fixer-psr2.php'
-    );
-    $c->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $c->import(SetList::SYMPLIFY);
+    $c->import(SetList::CLEAN_CODE);
+    $c->import(SetList::COMMON);
+    $c->import(SetList::PSR_12);
+    $c->import(SetList::PHPUNIT);
+    // $c->import(SetList::DOCTRINE_ANNOTATIONS);
+    // $c->import(SetList::PHP_CS_FIXER);
+    // $c->import(SetList::PHP_CS_FIXER_RISKY);
 
     $parameters = $c->parameters();
 
@@ -37,36 +33,32 @@ return static function (ContainerConfigurator $c): void {
             __DIR__ . '/tests',
         ]
     );
-    $parameters->set('indentation', '    ');
-    $parameters->set('line_length', 140);
-    $parameters->set('exclude_files', []);
+
+    $parameters->set(Option::INDENTATION, '    ');
+    $parameters->set(Option::PARALLEL, true);
 
     $parameters->set(
         Option::SKIP,
         [
-            // ToDo
-            UnusedParameterSniff::class . '.UnusedParameter' => null,
-            // ----------------------------------------------------------------
-            ParameterTypeHintSniff::class . '.MissingAnyTypeHint' => null,
-            ParameterTypeHintSniff::class . '.MissingAnyTypeHint' => null,
-            PropertyTypeHintSniff::class . '.MissingAnyTypeHint' => null,
-            ReturnTypeHintSniff::class . '.MissingAnyTypeHint' => null,
-            // ----------------------------------------------------------------
-            // TMP skip: not working correctly
-            MethodChainingIndentationFixer::class => null,
+            CommentedOutCodeSniff::class => null,
+            DisallowCommentAfterCodeSniff::class => null,
+            DisallowDirectMagicInvokeCallSniff::class => null,
+            NotOperatorWithSuccessorSpaceFixer::class => null,
             MethodChainingNewlineFixer::class => null,
             StandardizeHereNowDocKeywordFixer::class => null,
-            ParameterTypeHintSniff::class . '.MissingNativeTypeHint' => null,
-            PropertyTypeHintSniff::class . '.MissingNativeTypeHint' => null,
-            ReturnTypeHintSniff::class . '.MissingNativeTypeHint' => null,
-            // ----------------------------------------------------------------
-            ClassAttributesSeparationFixer::class => null,
-            NotOperatorWithSuccessorSpaceFixer::class => null,
-            DisallowCommentAfterCodeSniff::class . '.DisallowedCommentAfterCode' => null,
-            DisallowDirectMagicInvokeCallSniff::class . '.DisallowDirectMagicInvokeCall' => null,
-            ParameterTypeHintSniff::class . '.MissingTraversableTypeHintSpecification' => null,
-            PropertyTypeHintSniff::class . '.MissingTraversableTypeHintSpecification' => null,
-            ReturnTypeHintSniff::class . '.MissingTraversableTypeHintSpecification' => null,
         ]
     );
+
+    $services = $c->services();
+    $services->set(RequireTrailingCommaInDeclarationSniff::class);
+    $services->set(GeneralPhpdocAnnotationRemoveFixer::class)->call(
+        'configure',
+        [
+            [
+                'annotations' => [
+                ],
+            ],
+        ]
+    )
+    ;
 };
