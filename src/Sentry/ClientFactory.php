435808d65fcb824c1fb8d1749cacb15ff13b375b
David Solc <david@solc.dev>
2025-03-01 11:54:34 +0100

    Sentry: ClientFactory via init

--- src/Sentry/ClientFactory.php
+++ src/Sentry/ClientFactory.php
@@ -5,29 +5,26 @@ declare(strict_types=1);
 namespace Solcik\Sentry;
 
 use Jean85\PrettyVersions;
-use Sentry\ClientBuilder;
 use Sentry\ClientInterface;
-use Sentry\Transport\TransportFactoryInterface;
+use Sentry\SentrySdk;
 
 final class ClientFactory
 {
     /**
      * @param string[] $options
      */
-    public function create(
-        TransportFactoryInterface $transportFactory,
-        array $options,
-        string $name,
-    ): ClientInterface {
+    public function create(array $options, string $name): ClientInterface
+    {
         $version = PrettyVersions::getVersion($name);
 
         $options = array_merge($options, [
             'release' => $name . '@' . $version->getShortVersion(),
         ]);
 
-        $builder = ClientBuilder::create($options);
-        $builder->setTransportFactory($transportFactory);
+        \Sentry\init($options);
 
-        return $builder->getClient();
+        $hub = SentrySdk::getCurrentHub();
+
+        return $hub->getClient();
     }
 }
